b916abd2fc012ba7b2b44eb5bae14768902f0709
qmacias <ezequielqmacias@gmail.com>
2023-02-18 17:03:02 -0300

    Add Initial Files

--- src/main/java/org/qmacias/discovery/server/DiscoveryServerApplication.java
+++ src/main/java/org/qmacias/discovery/server/DiscoveryServerApplication.java
@@ -1,4 +1,4 @@
-package org.qmacias.eureka.demo;
+package org.qmacias.discovery.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/org/qmacias/discovery/server/DiscoveryServerApplicationTests.java
+++ src/test/java/org/qmacias/discovery/server/DiscoveryServerApplicationTests.java
@@ -1,4 +1,4 @@
-package org.qmacias.eureka.demo;
+package org.qmacias.discovery.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
